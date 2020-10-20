44706894e4f069b605faed8ff1fcb719b27c5475
MaxBromley1 <mjbro2003@gmail.com>
2020-10-20 14:40:10 +0000

    Fixed my randomness

--- index.java
+++ index.java
@@ -1,31 +1,34 @@
-background = 255;
+//background = 255;
+int rectX = (int) random(0,300);
+int rectY = (int) random(0,300);
+int rectH = (int) random(0,300);
+int rectW = (int) random(0,300);
 
-int xx = (int) random(0,100);
-int yy = (int) random(0,100);
-int w = (int) random(0,100);
-int h = (int) random(0,100);
-void mousePressed(){
-  background(255)
-  fill((int) random(0, 255), (int) random(0,(int) random(0,255)),255);
-  rect(mouseX + xx, mouseX + yy, mouseX + w, mouseY + h);
+int red = (int) random(0, 255);
+  int green = (int) random(0, 255);
+  int blue = (int) random(0, 255);
 
 
-}
 
 void setup() {
 	size(500, 500);
 }
 
 void draw() {
+stroke(0);
+ 
+int x = (int) random(mouseX + 20,mouseX - 20);
+int y = (int) random(mouseY + 20,mouseY - 20);
 
- int r = (int) random(0, 255);
+//println(mouseX, mouseY)
+  int r = (int) random(0, 255);
   int g = (int) random(0, 255);
   int b = (int) random(0, 255);
-
-int x = (int) random(0,60);
-int y = (int) random(0,60);
-
-
+fill(r,g,b);
+ellipse(x,y,10,10);
+ fill(red, blue, green);
+rect(rectX, rectY, rectH, rectW);
+}
 
 
 
@@ -41,8 +44,16 @@ int y = (int) random(0,60);
 // For example, “variable1 = variable1 + 2;”. 
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
-fill(r,g,b);
-ellipse(mouseX + x, mouseY + y,10,10);
 
-
+void mousePressed(){
+  red = random(0,255)
+green = random(0,255)
+blue = random(0,255)
+  background(255)
+  rectX = random(0,300)
+rectY = random(0,300)
+rectW = random(0,300)
+rectH = random(0,300)
 }
+
+
